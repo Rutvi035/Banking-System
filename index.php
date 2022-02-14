9d796cc5000bdfa7d6f172dbbf56bf2fda2a1c59
Rutvi <71960408+Rutvi035@users.noreply.github.com>
2022-02-14 10:05:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="main.css" type="text/css">
+    <title>HOME PAGE</title>
+    <style>
+
+        
+    </style>
+</head>
+<body>
+<div class="flexbox">
+     
+<form action="customer.php" method = "post">
+    
+    <div class="wrapper">
+        <h1> Transacting <br> Online</h1> 
+        <h3> Fill the monetary feels virtually </h3>
+        <button type="submit" name="submit" class="button"> <span> View Customers </span> </button>
+    </div>
+</form>
+
+<div class="img"></div>
+    <img style = " object-fit: cover; " src = "rr.jpg" height = 700px width = 3050px />
+</div>
+
+</body>
+</html>
